fd5232dec519c27189e0fc627266fc3d55534b8e
Xavier Dutreilh <xavier@dutreilh.com>
2017-03-01 20:53:08 +0100

    Switch to standard.js

--- test/generator_test.js
+++ test/generator_test.js
@@ -1,26 +1,26 @@
-'use strict';
+/* eslint-env jasmine */
 
-const fs = require('fs');
+const fs = require('fs')
 
-const expect = require('expect');
+const expect = require('expect')
 
-const generator = require('../lib');
+const generator = require('../lib')
 
-function read(filename) {
-  return JSON.parse(fs.readFileSync(filename, 'utf8'));
+function read (filename) {
+  return JSON.parse(fs.readFileSync(filename, 'utf8'))
 }
 
 describe('generator', () => {
   describe('generate', () => {
     it('should generate unique element IDs', () => {
-      const expected = read(`${__dirname}/expected/ids.json`);
-      const actual = [];
+      const expected = read(`${__dirname}/expected/ids.json`)
+      const actual = []
 
       for (let i = 1; i <= 36; i++) {
-        actual.push(generator.generate());
+        actual.push(generator.generate())
       }
 
-      expect(actual).toEqual(expected);
-    });
-  });
-});
+      expect(actual).toEqual(expected)
+    })
+  })
+})
