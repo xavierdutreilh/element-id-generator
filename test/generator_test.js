b990e813b21c81ae5afd7afdd7e218f3f4a7bbd2
Xavier Dutreilh <xavier@dutreilh.com>
2017-03-02 08:06:15 +0100

    Refactor file read op

--- test/generator_test.js
+++ test/generator_test.js
@@ -1,19 +1,13 @@
 /* eslint-env jasmine */
 
-const fs = require('fs')
-
 const expect = require('expect')
 
 const generator = require('../lib')
 
-function read (filename) {
-  return JSON.parse(fs.readFileSync(filename, 'utf8'))
-}
-
 describe('generator', () => {
   describe('generate', () => {
     it('should generate unique element IDs', () => {
-      const expected = read(`${__dirname}/expected/ids.json`)
+      const expected = require('./expected/ids')
       const actual = []
 
       for (let i = 1; i <= 36; i++) {
